7ca4aba06142e369bc10bb69559f541e053c19e5
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2016-09-03 17:26:22 +0200

    fixed docker build script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 0.0.3
+* fixed docker build script
+
 # 0.0.2
 * fixed docker image
 
--- travis-docker.sh
+++ travis-docker.sh
@@ -2,6 +2,6 @@
 set -ev
 if [ ! -z "$TRAVIS_TAG" ]; then 
 	docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-    docker build --build-arg tag=$TRAVIS_TAG -t triberraar/triber-chat-ui:latest -t triberraar/triber-chat-ui:$TRAVIS_TAG .
+    docker build -t triberraar/triber-chat-ui:latest -t triberraar/triber-chat-ui:$TRAVIS_TAG .
     docker push triberraar/triber-chat-ui
 fi
